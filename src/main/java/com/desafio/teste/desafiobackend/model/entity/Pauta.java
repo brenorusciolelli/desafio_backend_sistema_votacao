f8e3e6e0a8b13ab00dde3df96c1d19797f44de0f
breno brito <breno.brito.ext@spms.min-saude.pt>
2026-01-31 14:17:27 -0300

    feat: Create Pauta.java

---
+++ src/main/java/com/desafio/teste/desafiobackend/model/entity/Pauta.java
@@ -0,0 +1,22 @@
+package com.desafio.teste.desafiobackend.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Data;
+
+@Entity
+@Data
+public class Pauta {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  private String titulo;
+  private String descricao;
+
+  private LocalDateTime createdAt = LocalDateTime.now();
+}
