29b60769287ba5534228ee7575d1501eb02cf96b
breno brito <breno.brito.ext@spms.min-saude.pt>
2026-02-01 15:12:43 -0300

    feat: refactory.

--- src/main/java/com/desafio/teste/desafiobackend/service/SessionService.java
+++ src/main/java/com/desafio/teste/desafiobackend/service/SessionService.java
@@ -2,9 +2,12 @@ package com.desafio.teste.desafiobackend.service;
 
 import com.desafio.teste.desafiobackend.model.entity.PautaEntity;
 import com.desafio.teste.desafiobackend.model.entity.SessaoVotacaoEntity;
+import com.desafio.teste.desafiobackend.model.enums.VoteValue;
 import com.desafio.teste.desafiobackend.model.repository.PautaRepository;
 import com.desafio.teste.desafiobackend.model.repository.SessaoRepository;
+import com.desafio.teste.desafiobackend.model.repository.VoteRepository;
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,8 @@ import org.springframework.stereotype.Service;
 public class SessionService {
 
   private final SessaoRepository repository;
-  private final PautaRepository  pautaRepository;
+  private final PautaRepository pautaRepository;
+  private final VoteRepository  voteRepository;
 
   public SessaoVotacaoEntity startSession(UUID pautaId, Long minutos) {
     PautaEntity pauta = pautaRepository.findById(pautaId)
@@ -22,12 +26,23 @@ public class SessionService {
 
     SessaoVotacaoEntity sessao = new SessaoVotacaoEntity();
     sessao.setPauta(pauta);
-    sessao.setStart(LocalDateTime.now());
-    sessao.setEnd(LocalDateTime.now().plusMinutes(
+    sessao.setStartTime(LocalDateTime.now());
+    sessao.setEndTime(LocalDateTime.now().plusMinutes(
         minutos != null ? minutos : 1
     ));
 
     return repository.save(sessao);
   }
+
+  public Map<String, Long> calculateVotes(UUID id){
+    SessaoVotacaoEntity sessao = repository.findByPauta_Id(id)
+        .orElseThrow();
+
+    return Map.of(
+        "YES", voteRepository.countBySessionAndVoteValue(sessao, VoteValue.YES),
+        "NOT", voteRepository.countBySessionAndVoteValue(sessao, VoteValue.NOT)
+    );
+  }
+
 }
 
