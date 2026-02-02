fd757dc221b6bb9cd949480e95808cc43a15f662
breno brito <breno.brito.ext@spms.min-saude.pt>
2026-02-02 07:38:12 -0300

    feat: create BusinessException.

---
+++ src/main/java/com/desafio/teste/desafiobackend/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.desafio.teste.desafiobackend.exception;
+
+public class BusinessException extends RuntimeException {
+
+  public BusinessException(String message) {
+    super(message);
+  }
+}
+
